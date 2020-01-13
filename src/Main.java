37ed94ddb83f9dc7f47c66ed40fbb81a0479da1c
alekseydanilov <qnoize@gmail.com>
2020-01-13 14:36:41 +0300

    1st

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 
+//Write a function that takes in a string of one or more words, and returns the same string,
+//        but with all five or more letter words reversed (Just like the name of this Kata).
+//        Strings passed in will consist of only letters and spaces. Spaces will be included
+//        only when more than one word is present.
+//
+//        Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test")
+//        => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
 public class Main {
     public static void main(String[] args) {
         //String sentence = "Welcome";
@@ -6,24 +13,24 @@ public class Main {
         System.out.println(spinWords(sentence));
     }
 
-     public static String spinWords(String sentence) {
-         String result = "";
-         if (sentence.contains(" ")) {
-             String[] words = sentence.split(" ");
-             for (int i = 0; i < words.length; i++) {
-                 StringBuffer stringBuffer = new StringBuffer(words[i]);
-                 if (stringBuffer.length() >= 5) {
-                     stringBuffer.reverse();
-                 }
-                 result = result.concat(String.valueOf(stringBuffer));
-                 if (i < words.length - 1) {
-                     result = result + " ";
-                 }
-             }
-         } else {
-             StringBuffer stringBuffer = new StringBuffer(sentence);
-             result = stringBuffer.reverse().toString();
-         }
-         return result;
-     }
+    public static String spinWords(String sentence) {
+        String result = "";
+        if (sentence.contains(" ")) {
+            String[] words = sentence.split(" ");
+            for (int i = 0; i < words.length; i++) {
+                StringBuffer stringBuffer = new StringBuffer(words[i]);
+                if (stringBuffer.length() >= 5) {
+                    stringBuffer.reverse();
+                }
+                result = result.concat(String.valueOf(stringBuffer));
+                if (i < words.length - 1) {
+                    result = result + " ";
+                }
+            }
+        } else {
+            StringBuffer stringBuffer = new StringBuffer(sentence);
+            result = stringBuffer.reverse().toString();
+        }
+        return result;
+    }
 }
